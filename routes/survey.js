b76c429d85b933735ce32ff376f20bb1e2c0e8aa
dllamas01 <112565240+dllamas01@users.noreply.github.com>
2023-10-23 08:30:20 -0500

    new
    hi

--- routes/survey.js
+++ routes/survey.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const path = require("path");
 const { categories, questions, maximumPts, calculateScores } = require('../data/data.js');
 router.get('/notFound', (req, res) => {
-    console.log('Attempting to render About...');
     res.render('notFound', {
         pageTitle: 'survey',
         from: 'home',
